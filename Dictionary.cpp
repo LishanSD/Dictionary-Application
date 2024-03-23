0f033bedd2ce41fef13beb0face2030ed229d4f5
Lishan Dissanayake <dissanayake.lishan@gmail.com>
2024-03-24 00:00:00 +0530

    Class Dictionary

--- Dictionary.cpp
+++ Dictionary.cpp
@@ -4,3 +4,25 @@ using namespace std;
 
 // Constructor
 Dictionary::Dictionary() {}
+
+// Getters
+vector<Word> Dictionary::getWordlist()
+{
+    return this->wordlist;
+}
+
+int Dictionary::getHighScore()
+{
+    return this->highScore;
+}
+
+// Setters
+void Dictionary::setWordlist(Word word)
+{
+    this->wordlist.push_back(word);
+}
+
+void Dictionary::setHighScore(int newHighScore)
+{
+    this->highScore = newHighScore;
+}
