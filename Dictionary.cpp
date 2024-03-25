021b04e1713b7efb022947e0875bcba97193a34a
Lishan Dissanayake <dissanayake.lishan@gmail.com>
2024-03-26 00:00:00 +0530

    Class Dictionary

--- Dictionary.cpp
+++ Dictionary.cpp
@@ -2,6 +2,27 @@
 
 using namespace std;
 
+string format_string(string word)
+{
+    string format_word;
+
+    // Make all the characters lowercase
+    for (int i = 0; i < word.length(); i++)
+    {
+        format_word += tolower(word[i]);
+    }
+
+    // Replace the spaces in the word by hyphens
+    size_t start = 0;
+
+    while ((format_word.find(" ", start)) != string::npos)
+    {
+        size_t x = format_word.find(" ", start);
+    }
+
+    return format_word;
+}
+
 // Constructor
 Dictionary::Dictionary() {}
 
